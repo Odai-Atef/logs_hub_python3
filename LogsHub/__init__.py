ca4e16c8a7d1b84608315c9e39ca5fd7c7bd7b41
odai atef <oatef@css.org.sa>
2020-11-01 12:10:59 +0300

    add tags

--- LogsHub/__init__.py
+++ LogsHub/__init__.py
@@ -18,13 +18,16 @@ con.setLevel(logging.INFO)
 formatter = logging.Formatter('%(message)s')
 con.setFormatter(formatter)
 logger.addHandler(con)
+tags={}
+def set_tags(key,value):
+        tags[key]=value
 
 def _log(msg, application, level,execution_time, user_id, extra_data):
         now =str( int(datetime.now().timestamp()))    
         environment = 'development' if config("ENVIRONMENT") == None or config("ENVIRONMENT")=="" else config("ENVIRONMENT")
         application = 'unknown' if config("APPLICATION") == None or config("APPLICATION")=="" else config("APPLICATION")
 
-        data={
+        original_data={
             "message": msg,
             "level": level,
             "application": application,
@@ -34,14 +37,14 @@ def _log(msg, application, level,execution_time, user_id, extra_data):
             "extra_data": extra_data,
             "timestamp": now
         }
+        data={**original_data, **tags}
 
         handler = logging.FileHandler(config('DIR')+now+'.log')
         handler.setLevel(logging.INFO)
         formatter = logging.Formatter('%(message)s')
         handler.setFormatter(formatter)
         logger.addHandler(handler) 
-
-        logger.warning(json.dumps(data))
+        logger.warning(json.dumps(data)+"\n")
         logger.removeHandler(handler)
 
         return data
