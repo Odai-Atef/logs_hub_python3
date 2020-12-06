03a8168f6a33cf6f438ca731c351d065bd173185
odai atef <oatef@css.org.sa>
2020-12-06 15:59:24 +0300

    1.1.5

--- LogsHub/__init__.py
+++ LogsHub/__init__.py
@@ -7,21 +7,28 @@ import os
 
 	
 dir_path = os.path.abspath(os.curdir)
+extra_tags={}
 config = AutoConfig(search_path=dir_path)
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-con = logging.StreamHandler()
-con.setLevel(logging.INFO)
+# con = logging.StreamHandler()
+# con.setLevel(logging.INFO)
+# formatter = logging.Formatter('%(message)s')
+# con.setFormatter(formatter)
+# logger.addHandler(con)
 
-formatter = logging.Formatter('%(message)s')
-con.setFormatter(formatter)
-logger.addHandler(con)
-tags={}
 def set_tags(key,value):
-        tags[key]=value
-
+        extra_tags[key]=value
+def get_tags(key):
+        if key in extra_tags:
+                return  extra_tags[key]
+        return None
+def merge_two_dicts(x, y):
+        z = x.copy()   # start with x's keys and values
+        z.update(y)    # modifies z with y's keys and values & returns None
+        return z
 def _log(msg, application, level,execution_time, user_id, extra_data):
         now =str( int(datetime.now().timestamp()))    
         environment = 'development' if config("ENVIRONMENT") == None or config("ENVIRONMENT")=="" else config("ENVIRONMENT")
@@ -37,7 +44,7 @@ def _log(msg, application, level,execution_time, user_id, extra_data):
             "extra_data": extra_data,
             "timestamp": now
         }
-        data={**original_data, **tags}
+        data=merge_two_dicts(original_data, extra_tags)
 
         handler = logging.FileHandler(config('DIR')+now+'.log')
         handler.setLevel(logging.INFO)
